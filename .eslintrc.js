b75d766ee42e481b2004092b0b405bc50d590ff7
Jim Sowers <jpsowers@gmail.com>
2019-05-30 13:34:24 -0500

    now pulling data from the mongo db and returning correctly to browser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
+
 module.exports = {
-    "extends": "airbnb-base"
-  };
+  extends: 'airbnb-base',
+  rules:{
+    "linebreak-style": 0,
+    'prefer-destructuring': 0,
+  }
+};
--- app.js
+++ app.js
@@ -4,9 +4,24 @@ const chalk = require('chalk'); // coloring for console text
 const debug = require('debug')('app'); // safer logging than using console - only runs in debug... (don't use console)
 const morgan = require('morgan'); // http logging
 const path = require('path'); // helps create valid path to html pages
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.port || 4000;
+const db = mongoose.connect('mongodb://localhost/nodeApi');
+const Todo = require('./models/todoModel');
+
+const todoRouter = express.Router();
+
+todoRouter.get('/todos', (req, res) => {
+  Todo.find((err, todos) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(todos);
+  });
+});
+app.use('/api', todoRouter);
 
 app.use(morgan('combined')); // set morgan to 'tiny' to get much more succint logging
 app.use(express.static(path.join(__dirname, 'public')));
@@ -20,6 +35,7 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   res.render('index', { endpoints: ['first endpoint', 'second endpoint'], title: 'API test endpoint in node.js' });
 });
+
 
 app.listen(port, () => {
   debug(`listening on port: ${chalk.green(port)}`);
